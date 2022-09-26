d3dfcd053d31918dd3866f6a0ce8b6e75d476c99
Raphael Alampay <raphael.alampay@gmail.com>
2022-09-26 15:06:56 +0800

    Added migration for Token and LastLogin

---
+++ Migrations/20220926070551_AddedTokenAndLastLoginForUsers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankApi.Migrations
+{
+    public partial class AddedTokenAndLastLoginForUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLogin",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastLogin",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "Users");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -5,4 +5,6 @@ public class User
     public Int32 Id { get; set; }
     public string Username { get; set; }
     public string EncryptedPassword { get; set; }
+    public string ?Token { get; set; }
+    public DateTime ?LastLogin { get; set; }
 }
